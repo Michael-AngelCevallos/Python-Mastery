c92a70747315296eb8eac40acc43e5cb487e3aba
Michael-AngelCevallos <michael.a.cevallos@gmail.com>
2026-07-07 18:13:51 -0500

    Practice for Operators Started

---
+++ 01_Fundamentals/02_Data_Types/Practice/01_Operators_Practice/01_Weekly_Pay.py
@@ -0,0 +1,30 @@
+# ======================================================
+# Practice Range
+# Phase 1 - Python Fundamentals
+# Mission 1.3 - Operators
+
+# Practice 01 - Weekly Pay
+
+
+
+
+# Scenario:
+
+#! A construction company wants a simple payroll program.
+
+#! The employee's hourly wage and the total hours worked
+#! this week are already known and stored in the program.
+
+#! The program should calculate the employee's total weekly
+#! pay and display the result.
+
+#! Assume all values are valid.
+
+# ======================================================
+
+em_hr_wage = 25.35
+em_total_hr_worked = 40
+
+total_wk_pay = em_hr_wage * em_total_hr_worked
+
+print(f"This Employee's total weekly pay is: ${total_wk_pay:.2f}")
